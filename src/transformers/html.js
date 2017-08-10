9c6efcde583282a1a1c017f0b89de401ef349287
Nik Coughlin <nrkn.com@gmail.com>
2017-08-11 11:57:21 +1200

    HTML components should always return a fragment

--- src/transformers/html.js
+++ src/transformers/html.js
@@ -7,7 +7,18 @@ const { parse } = domUtils
 const Html = options => {
   const { document } = options
 
-  const html = str => parse( document, str )
+  const html = str => {
+    const node = parse( document, str )
+
+    if( node.nodeName === '#document-fragment' )
+      return node
+
+    const fragment = document.createDocumentFragment()
+
+    fragment.appendChild( node )
+
+    return fragment
+  }
 
   return html
 }
