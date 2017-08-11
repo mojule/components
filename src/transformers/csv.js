79b4b45db42f4e319bed98ed461e63da75a51846
Nik Coughlin <nrkn.com@gmail.com>
2017-08-11 18:31:35 +1200

    Separate table/models when csv source

--- src/transformers/csv.js
+++ src/transformers/csv.js
@@ -1,20 +1,29 @@
 'use strict'
 
 const Grid = require( '@mojule/grid' )
+const flatten = require( '@mojule/flatten' )
+
+const { expand } = flatten
+
+const gridToTableModel = grid => {
+  const columnNames = grid.getColumnNames().map( name => ({ name }))
+
+  const cell = ( value, index ) => ({ name: columnNames[ index ], value })
+  const row = value => ({ cells: value.map( cell ) })
+
+  const rows = grid.getRows().map( row )
+
+  return { columnNames, rows }
+}
 
 const Csv = () => {
   const csv = str => {
     const grid = Grid( str )
 
-    const columnNames = grid.getColumnNames()
-    const rows = grid.getRows()
-    const nameValueRows = rows.map( row =>
-      row.map( ( value, index ) => ({ name: columnNames[ index ], value }) )
-    )
-    // should possibly be mapped via mojule/flatten/expand
-    const models = grid.models()
+    const table = gridToTableModel( grid )
+    const models = grid.models().map( model => expand( model ) )
 
-    return { columnNames, rows, nameValueRows, models }
+    return { table, models }
   }
 
   return csv
